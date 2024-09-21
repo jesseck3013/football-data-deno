ef26e9004245484724e45e787cdc745a97e9e7aa
jesseck3013 <jesseck3013@outlook.com>
2024-09-21 10:01:47 +0800

    use ts modifier over js modifier

--- src/client.ts
+++ src/client.ts
@@ -19,29 +19,29 @@ import {
 import { Filters, MatchFilters } from "./types.ts";
 
 export class Client {
-  #authFetch: (url: URL) => Promise<Response>;
+  private authFetch: (url: URL) => Promise<Response>;
   constructor(token: string) {
-    this.#authFetch = makeAuthFetchFn(token);
+    this.authFetch = makeAuthFetchFn(token);
   }
 
   /** list all areas */
   areas() {
-    return this.#authFetch(areas());
+    return this.authFetch(areas());
   }
 
   /** given a parent id, return all child ids */
   areasById(id: number | string) {
-    return this.#authFetch(areasById(id));
+    return this.authFetch(areasById(id));
   }
 
   /** list competitions */
   competitions(filters: Filters = {}) {
-    return this.#authFetch(competitions(filters));
+    return this.authFetch(competitions(filters));
   }
 
   /** info of a specific competition by its id or code */
   competition(idOrCode: number | string = "PL", filters: Filters = {}) {
-    return this.#authFetch(competition(idOrCode, filters));
+    return this.authFetch(competition(idOrCode, filters));
   }
 
   /** standings info of a specific competition */
@@ -49,7 +49,7 @@ export class Client {
     idOrCode: number | string = "PL",
     filters: Filters = {},
   ) {
-    return this.#authFetch(standingsOfCompetition(idOrCode, filters));
+    return this.authFetch(standingsOfCompetition(idOrCode, filters));
   }
 
   /** top scorer info of a specific competition */
@@ -57,7 +57,7 @@ export class Client {
     idOrCode: number | string = "PL",
     filters: Filters = {},
   ) {
-    return this.#authFetch(scorersOfCompetition(idOrCode, filters));
+    return this.authFetch(scorersOfCompetition(idOrCode, filters));
   }
 
   /** matches of a competition */
@@ -65,46 +65,46 @@ export class Client {
     idOrCode: number | string = "PL",
     filters: Filters = {},
   ) {
-    return this.#authFetch(matchesOfCompetition(idOrCode, filters));
+    return this.authFetch(matchesOfCompetition(idOrCode, filters));
   }
 
   /** teams info of its competition */
   teamsOfCompetition(idOrCode: number | string = "PL", filters: Filters = {}) {
-    return this.#authFetch(teamsOfCompetition(idOrCode, filters));
+    return this.authFetch(teamsOfCompetition(idOrCode, filters));
   }
 
   /** match info */
   matches(filters: MatchFilters = { ids: [] }) {
-    return this.#authFetch(match(filters));
+    return this.authFetch(match(filters));
   }
 
   /** head2head history info of a match */
   head2head(matchId: number, filters: Filters = {}) {
-    return this.#authFetch(head2headOfMatch(matchId, filters));
+    return this.authFetch(head2headOfMatch(matchId, filters));
   }
 
   /** list all teams */
   teams() {
-    return this.#authFetch(teams());
+    return this.authFetch(teams());
   }
 
   /** team info */
   team(id: number) {
-    return this.#authFetch(team(id));
+    return this.authFetch(team(id));
   }
 
   /** matches info of a team */
   matchesOfTeam(id: number, filters: Filters = {}) {
-    return this.#authFetch(matchesOfTeam(id, filters));
+    return this.authFetch(matchesOfTeam(id, filters));
   }
 
   /** person info */
   person(id: number) {
-    return this.#authFetch(person(id));
+    return this.authFetch(person(id));
   }
 
   /** matches of a person */
   matchesOfPerson(id: number, filters: Filters = {}) {
-    return this.#authFetch(matchesOfPerson(id, filters));
+    return this.authFetch(matchesOfPerson(id, filters));
   }
 }
